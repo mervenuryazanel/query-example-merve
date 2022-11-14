24aeaf5f744d07eb1e7c2d42883d3f50e37baed3
merve nur yazanel <mervenuryazanel@gmail.com>
2022-11-14 16:50:25 +0300

    add react-query-dev-tools configuration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import { QueryClientProvider, QueryClient } from 'react-query';
+import {ReactQueryDevtools} from "react-query/devtools";
 import './App.css'
 import { HomePage } from './components/Home.page'
 import { RQSuperHeroesPage } from './components/RQSuperHeroes.page'
@@ -41,6 +42,7 @@ function App() {
           </Routes>
         </div>
       </Router>
+      <ReactQueryDevtools initialIsOpen={false} position='bottom-right'/>
     </QueryClientProvider>
 
   )
--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -6,7 +6,7 @@ import axios from "axios";
 import { useQuery } from "react-query";
 
 const fetchSuperHeroes=()=>{
-  return axios.get("http://localhost:4000/superheroasds")
+  return axios.get("http://localhost:4000/superheros")
 }
 export  function RQSuperHeroesPage() {
   

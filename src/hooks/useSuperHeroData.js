ae81e604237440ed1288a0293611c98ca6de8ecd
merve nur yazanel <mervenuryazanel@gmail.com>
2022-11-15 16:49:21 +0300

    a better way about query with id

--- src/hooks/useSuperHeroData.js
+++ src/hooks/useSuperHeroData.js
@@ -1,9 +1,20 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 
-const fetchSuperHero = (heroId) => {
+// const fetchSuperHero = (heroId) => {
+//     return axios.get(`http://localhost:4000/superheros/${heroId}`)
+// }
+// export const useSuperHeroData = (heroId) => {
+//     return useQuery(['super-hero', heroId], () => fetchSuperHero(heroId));
+// }
+
+// for a better practise read the following code
+
+const fetchSuperHero = ({ queryKey }) => {
+    const heroId = queryKey[1];
+    console.log("query key:", queryKey);
     return axios.get(`http://localhost:4000/superheros/${heroId}`)
 }
 export const useSuperHeroData = (heroId) => {
-    return useQuery(['super-hero', heroId], () => fetchSuperHero(heroId));
+    return useQuery(['super-hero', heroId], fetchSuperHero);
 }

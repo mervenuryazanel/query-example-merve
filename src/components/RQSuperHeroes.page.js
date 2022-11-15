49618f7b82e3c0ed77859aebf191f5cf8276bfbd
merve nur yazanel <mervenuryazanel@gmail.com>
2022-11-15 11:45:52 +0300

    a practise on conditional fetching

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -3,26 +3,36 @@
 //react query abstracts away all the useState and useEffect and their effects in the data fetch
 
 import axios from "axios";
+import { useState } from "react";
 import { useQuery } from "react-query";
 
 const fetchSuperHeroes = () => {
   return axios.get("http://localhost:4000/superheros")
 }
 export function RQSuperHeroesPage() {
-  const onSuccess = (data) => {
-    console.log("Data fetching is successfull.", data)
+  const [shouldFetch, setShouldFetch] = useState(true);
+
+  const onSuccess = ({ data }) => {
+    console.log("Data fetching is successfull.", data);
+    if (data.length === 4) {
+      //stop polling
+
+      setShouldFetch(false); //if there will be 4 heros stop the polling
+    }
   }
 
   const onError = (error) => {
     console.log("Error while data fetcing", error);
+    //stop polling
+    setShouldFetch(false); //if there will be an error stop the polling
+
   }
 
-  const { isLoading, data, isError, error, isFetching } = useQuery(
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     'super-heroes',
     fetchSuperHeroes,
     {
-      // onSuccess: onSuccess,
-      // onError: onError 
+      refetchInterval: shouldFetch ? 2000 : false, //if the shouldFetch true then refetch every two seconds, else don't refetch
       onSuccess,
       onError
     }
